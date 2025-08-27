62b06651bf5b9ea1100e0efb21a01ae620701d6f
Rimante Awdisson <rimante_macbook@Rimantes-MacBook-Pro.local>
2025-08-27 10:54:26 +0200

    added resume

---
+++ app/CV/page.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const CVPage = () => {
+  return (
+    <main style={{ display: "flex", flexDirection: "column", alignItems: "center", padding: "2rem" }}>
+      <h1 style={{
+            fontSize: '3rem',
+            fontWeight: 'bold',
+            textAlign: 'left',
+            color: '#374151',
+            fontFamily: 'Syne, sans-serif',
+            marginBottom: '1rem'
+      }}>Resume</h1>
+      <img
+        src="/Rimante_Awdisson.png"
+        alt="Rimante Awdisson CV"
+        style={{ maxWidth: "40%", height: "auto", border: "1px solid #ccc", borderRadius: "8px" }}
+      />
+    </main>
+  );
+};
+
+export default CVPage;
