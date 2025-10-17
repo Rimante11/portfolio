234790ab9f259a8ca980435c36e14926ef74491f
Rimante Awdisson <rimante_macbook@Rimantes-MacBook-Pro.local>
2025-10-17 16:11:41 +0200

    wrong image

--- app/CV/page.tsx
+++ app/CV/page.tsx
@@ -12,7 +12,7 @@ const CVPage = () => {
             marginBottom: '1rem'
       }}>Resume</h1>
       <img
-        src="/ra_cv.png"
+        src="/ra.png"
         alt="Rimante Awdisson CV"
         style={{ maxWidth: "40%", height: "auto", border: "1px solid #ccc", borderRadius: "8px" }}
       />
